9e58c4ad2af0d8e7194176a3abc6612c589cee8a
Ubuntu <ubuntu@ip-172-31-57-127.ec2.internal>
2017-08-06 18:16:28 +0000

    [+] bash script for linux

---
+++ example.bash
@@ -0,0 +1,2 @@
+export SENTRY_MONITORING_PRIVATE_DSN=http://9d6a8682d7254f19b073af0c509862a5:38c58938e8db44828134b535ffee0bcc@granumsalis.ga:9000/8
+python3 monitoring.py -host granumsalis.ga -t 423531733:AAH1mSwyZ1Yt0OgdGxTQDmX0bGrh283_i18 -u 1562163
